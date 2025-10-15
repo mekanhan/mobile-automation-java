ac428e1f6499bdee5c286265251ddac0390eff53
mekan hanov <mekjanhan@gmail.com>
2025-10-15 10:17:16 -0500

    designed optimized cross platform structure

---
+++ src/test/java/tests/steps/App1Steps.java
@@ -0,0 +1,10 @@
+public class App1Steps {
+    @When("I login with {string} and {string}")
+    public void login(String user, String pass) {
+        if (isIOS()) {
+            // iOS specific implementation
+        } else {
+            // Android specific implementation
+        }
+    }
+}
